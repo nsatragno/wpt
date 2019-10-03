c18ed4d929a43c0cae88746c59d35a3a0aa138eb
Philip Jägenstedt <philip@foolip.org>
2019-10-03 14:21:00 +0200

    fixup! Capture screenshots/video from the macOS VMs (for fun)

--- tools/ci/azure/screencapture.sh
+++ tools/ci/azure/screencapture.sh
@@ -7,14 +7,14 @@ if [ "$VIDEO" = "true" ]; then
   # capture 10 seconds of video every 10 minutes for at most 2 hours
   seq 12 | while read counter; do
     NOW=`date -u "+%Y-%m-%d_%H-%M-%S"`
-    screencapture -V 10 "$PREFIX_$NOW.mov"
+    screencapture -V 10 "$PREFIX$NOW.mov"
     sleep 590
   done
 else
   # capture screenshots every 10 seconds for at most 2 hours
   seq 720 | while read counter; do
     NOW=`date -u "+%Y-%m-%d_%H-%M-%S"`
-    screencapture -t jpg "$PREFIX_$NOW.jpg"
+    screencapture -t jpg "$PREFIX$NOW.jpg"
     sleep 10
   done
 fi
